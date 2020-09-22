e25bc39f1dc142db8aa5acc05ca807001d2674fa
Minh Quyền <mquyennn@gmail.com>
2020-09-22 21:48:54 +0700

    buoi_0_taoComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { Buoi1Component } from './buoi1/buoi1.component';
+import { FooterComponent } from './buoi1/footer/footer.component';
+import { HeaderComponent } from './buoi1/header/header.component';
+import { ContentsComponent } from './buoi1/contents/contents.component';
+import { SideBarComponent } from './buoi1/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent // Các component sử dụng ( lẫn nha) trong module này
+    DemoComponent,
+    Buoi1Component,
+    FooterComponent,
+    HeaderComponent,
+    ContentsComponent,
+    SideBarComponent // Các component sử dụng ( lẫn nha) trong module này
   ],
   imports: [
     BrowserModule // Nơi để import khác vào modeul này
